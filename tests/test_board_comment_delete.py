d3bb583db07b5cc97edcd184831f35b049e24677
ji-ho lee <search5@gmail.com>
2024-04-04 01:56:48 +0900

    코멘트 삭제 버튼은 동적으로 생성되기 때문에 안 보여지는게 당연함

--- tests/test_board_comment_delete.py
+++ tests/test_board_comment_delete.py
@@ -1,24 +1,3 @@
-from bs4 import BeautifulSoup
-
-
-def test_guest_comment_delete_input(client, write_comment):
-    """게시물 댓글 삭제 버튼이 있는지 확인"""
-    article_id = write_comment.get("article_id")
-
-    req_http = client.get(f'/board/{article_id}')
-
-    # HTTP 응답 상태 확인
-    assert req_http.status == '200 OK', 'URL이 구현되지 않았습니다'
-
-    response_content = req_http.get_data()
-
-    response_parser = BeautifulSoup(response_content, 'lxml')
-
-    # 댓글 삭제 버튼 확인
-    comment_delete_btn = response_parser.select_one("btn.btn-danger.comment_delete")
-    assert comment_delete_btn is not None, '코멘트 삭제 버튼이 없습니다'
-
-
 def test_guest_comment_delete(client, write_comment):
     """손님 상태에서 게시물 댓글 삭제 기능 확인"""
     article_id = write_comment.get("article_id")
